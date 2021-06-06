2ddb0f7d9bc835e057eff019f76777df97010245
AJ <79541631+TheAJManagement@users.noreply.github.com>
2021-06-06 23:06:30 +0100

    Dynamic Display

--- assets/js/dynamicDisplay.js
+++ assets/js/dynamicDisplay.js
@@ -1,4 +1,4 @@
-import { profiles } from "./profiles.js";
+import { profiles } from "./profilesConfig.js";
 
 const profilesParent = document.querySelector('.section-center');
 const btnsContainer = document.querySelector('.btn-container');
@@ -10,52 +10,89 @@ window.addEventListener('DOMContentLoaded', () => {
 
 function displayFreelancers(profiles) {
     let freelancers = profiles.map((freelancer) => {
-        return `<article class="profile">
-                <img src="${freelancer.img}" class="profile-img" alt="Profile Picture of ${freelancer.name}">
-                <div class="profile-info">
-                    <div class="heading">
-                        <h3>${freelancer.name}</h3>
-                        <h4 class="department">${freelancer.department}</h4>
+        const profile = freelancer.department;
+        if (typeof profile === 'string') { // If freelancer has one department
+            return `<article class="profile">
+                    <img src="${freelancer.img}" class="profile-img" alt="Profile Picture of ${freelancer.name}">
+                    <div class="profile-info">
+                        <div class="heading">
+                            <h3>${freelancer.name}</h3>
+                            <h4 class="department">${freelancer.department}</h4>
+                        </div>
+                        <p>Total Commissions: ${freelancer.completed}</p>
+                        <p>Average Rating: ${freelancer.avgRating} (${freelancer.totalRatings})</p>
+                        <button class="btn">view profile</button>
                     </div>
-                    <p>Total Commissions: ${freelancer.completed}</p>
-                    <p>Average Rating: ${freelancer.avgRating} (${freelancer.totalRatings})</p>
-                    <button class="btn">view profile</button>
-                </div>
-            </article>`;
+                </article>`;
+        } else if (typeof profile === 'object') { // If freelancer has more than one departments
+            return `<article class="profile">
+                    <img src="${freelancer.img}" class="profile-img" alt="Profile Picture of ${freelancer.name}">
+                    <div class="profile-info">
+                        <div class="heading">
+                            <h3>${freelancer.name}</h3>
+                            <h4 class="department">${freelancer.department.join(', ')}</h4>
+                        </div>
+                        <p>Total Commissions: ${freelancer.completed}</p>
+                        <p>Average Rating: ${freelancer.avgRating} (${freelancer.totalRatings})</p>
+                        <button class="btn">view profile</button>
+                    </div>
+                </article>`;
+        }
     });
-    freelancers = freelancers.join('');
-    profilesParent.innerHTML = freelancers;
+    const randomizedOrder = freelancers // Randomizes the order of profiles
+        .map((a) => ({sort: Math.random(), value: a}))
+        .sort((a, b) => a.sort - b.sort)
+        .map((a) => a.value)
+        .join('');
+    profilesParent.innerHTML = randomizedOrder;
 }
 
 function displayButtons() {
+
     const departments = profiles.reduce((values, profile) => {
-        if (!values.includes(profile.department)) {
-            values.push(profile.department);
+        if (typeof profile.department === 'string') { // If freelancer has one department
+            if (!values.includes(profile.department)) {
+                values.push(profile.department);
+            }
+        } else if (typeof profile.department === 'object') { // If freelancer has more than one department
+            for (let i in profile.department) {
+                if (!values.includes(profile.department[i])) {
+                  values.push(profile.department[i]);
+                }
+            }
         }
         return values;
     }, ['all']);
 
-    const departmentBtns = departments.map((currentDepartment) => {
-        return `<button class="btn filter-btn" type="button" data-id="${currentDepartment}">
-                    ${currentDepartment}
-                </button>`;
+    const departmentBtns = departments.map((currentDepartment) => { // Renders buttons for each department category within profiles. One button per category.
+        return `<button class="btn filter-btn" type="button" data-id="${currentDepartment}">${currentDepartment}</button>`;
     }).join('');
     btnsContainer.innerHTML = departmentBtns;
-    const filterBtns = btnsContainer.querySelectorAll('.filter-btn');
 
+    const filterBtns = btnsContainer.querySelectorAll('.filter-btn');
     filterBtns.forEach((btn) => {
         btn.addEventListener('click', (e) => {
             const department = e.currentTarget.dataset.id;
-            const departmentCategory = profiles.filter((currentDepartment) => {
-                if (currentDepartment.department === department) {
-                    return currentDepartment;
-                }
-            });
 
-            if (department === 'all') {
+            const departmentCategory = profiles.filter((currentProfile) => {
+                const profile = currentProfile.department;
+                if (typeof profile === 'string') { // If freelancer has one department
+                    if (profile === department) {
+                        return currentProfile;
+                    }
+                } else if (typeof profile === 'object') { // If freelancer has more than one department
+                    for (let i in profile) {
+                      if (profile[i] === department) {
+                        console.log();
+                        return profile[i];
+                      }
+                    }
+                }
+            });           
+            if (department === 'all') { // Displays all profiles (All Button)
                 displayFreelancers(profiles);
             } else {
-                displayFreelancers(departmentCategory);
+                displayFreelancers(departmentCategory); // Displays selected button category
             }
         });
     });
