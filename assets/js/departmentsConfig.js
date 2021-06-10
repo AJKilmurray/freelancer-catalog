fb7a82f5a5e339d921993e2e5a3691a16109e18a
AJ <79541631+TheAJManagement@users.noreply.github.com>
2021-06-10 23:09:32 +0100

    Departments Config

---
+++ assets/js/departmentsConfig.js
@@ -0,0 +1,71 @@
+/* Blueprint
+
+    {
+        id: 1,
+        department: [Departments],
+        specialties: [Specialties],
+        specialtyDescription: [Specialties Descriptions],
+        numOfFreelancers: [Number of freelancers with role] 
+    }
+
+*/
+
+
+export const departments = [
+    {
+        id: 1,
+        department: ['Development'],
+        specialties: ['Bot Developer', 'Plugin Developer'],
+        specialtyDescription: ["Our Bot Developers specialise in creating discord bots for you and your server members to utilise. Whether you need a bot for entertainment, administration, management, or general-purpose, a bot developer will be able to create a unique discord bot according to your request.", 'Plugin Developers specialise in creating plugins for your minecraft server. Surprise your playerbase with plugins specialised to your request.'],
+        numOfFreelancers: [38, 39], 
+    },
+    {
+        id: 2,
+        department: ['Building'],
+        specialties: ['Builder', 'Builder+', 'Terraformer', 'Organic'],
+        specialtyDescription: ['Builders specialise in using their creativity to create minecraft builds based on a reference, or idea. Any theme, size, or style will be created by our talented builders.', 'Builder+ is given to the highest quality builders, with a portfolio to show for it. The budget for Builder+ starts at $250.', "Terraformers create large generated maps, and terrain. If you need a large amount of customized world generation, terraformers will provide you with exactly what you're looking for.", 'Organics are the creation of figures, statues, and animals using minecraft blocks. Our talented organic builders will be able to produce a uniquely styled build of whatever creature, or figure you need.'],
+        numOfFreelancers: [38, 11, 4, 19], 
+    },
+    {
+        id: 3,
+        department: ['Design'],
+        specialties: ['GFX Designer', 'Illustrator', 'Vector Designer'],
+        specialtyDescription: ['GFX Designers use tools to create modern, sharp, and high quality designs for all needs. Some examples being: Logos, Banners, and Threads.', "Illustrators specialise in hand-drawn design. If you're looking for a hand-drawn style of design, Illustrators are what you're looking for.", 'Vector Designers specialise in simplistic, yet high-quality vector style designs.'],
+        numOfFreelancers: [23, 18, 13], 
+    },
+    {
+        id: 4,
+        department: ['Web'],
+        specialties: ['Web Designer', 'Web Developer', 'BuyCraft Designer', 'Xenforo Designer', 'UIX Designer', 'Pterodactyl Designer'],
+        specialtyDescription: ['Web Designers specialise in designing the front-end of your website. They are responsible for creating the design of your site for everyone to see.', 'Web Developers work on the back-end of your website. Almost every site is expected to have a backend to transfer data from the user to the web server.', 'BuyCraft Designers will make your buycraft store appear lively, professional and sleek by creating a design with your desired look.', 'Xenforo Designers specialise in styling your xenforo forum. Anything that can be customized, will be customized at your request.', 'UIX Designers are the backbone for all designs. Without a reference, wireframe, or sketch, it will make the design of your site take a longer process.', 'Pterodactyl Designers will give your personal pterodactyl panel a custom design for you to relish in.'],
+        numOfFreelancers: [8, 7, 3, 3, 9, 1], 
+    },
+    {
+        id: 5,
+        department: ['Setups'],
+        specialties: ['Server Setup', 'Configurator', 'Discord Setup', 'System Admin', 'BuyCraft Setups', 'Xenforo Setups'],
+        specialtyDescription: ['Server Setup specialists will be able to setup your entire server from scratch. They know how to optimise your server, work with the best plugins, and have knowledge of the best trends in the market to benefit your server.', 'Configurators will spice up your server with custom configuration of your plugins.', 'Discord Setup specialists will setup your discord server, from channels, roles, permissions, to bot configuration.', "System Admins specialise in the back-end setup of your server. If you're unsure about what's best for your server, our system admins will show you.", 'BuyCraft Setup specialists will setup your buycraft store.', 'Xenforo Setup specialists will setup your xenforo forum.'],
+        numOfFreelancers: [16, 27, 11, 5, 4, 1],
+    },
+    {
+        id: 6,
+        department: ['Video'],
+        specialties: ['Cinematograph', 'Video Editor', 'Trailer Creator', 'Intro Creator', 'Motion', 'Animator'],
+        specialtyDescription: ['Cinematographers specialise in motion picture films.', 'Video Editors will take your video, make custom edits, transitions, and special effects to engage your target audience.', 'Trailer Creators will film and edit a trailer for your minecraft server.', 'Intro Creators will create a custom intro for all of your social media needs.', 'Motion specialists know how to incorporate movement with design, creating designs with animated effects, and transitions.', 'Animators specialise in creating an illusion of movement, also known as animation.'],
+        numOfFreelancers: [6, 18, 12, 4, 10, 2],
+    },
+    {
+        id: 7,
+        department: ['Minecraft Design'],
+        specialties: ['Model Designer', 'Texture Pack Designer', 'Skin Designer', 'Render Artist'],
+        specialtyDescription: ['Model Designers are extremely talented designers that specialise in creating in-game models. This can be any sort of custom mob.', 'Texture Pack Designers create texture designs and combine them into an entire custom made texture pack.', 'Skin Designers create minecraft skin models for you to apply to your minecraft character.', 'Render Artists produce beautiful renders of your minecraft world. Typically, render artists are used for getting an amazing image of minecraft builds.'],
+        numOfFreelancers: [3, 2, 2, 8],
+    },
+    {
+        id: 8,
+        department: ['Writing'],
+        specialties: ['Writer'],
+        specialtyDescription: ['Writers will produce any sort writing piece to fit your needs. Personal, education, legal, anything.'],
+        numOfFreelancers: [9],
+    }   
+];
