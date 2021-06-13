daa46d9122342b0a49fff71a700c15900647cac2
AJ <79541631+TheAJManagement@users.noreply.github.com>
2021-06-13 14:22:18 +0100

    Visual bug fix, removal of answer top border

--- assets/js/faqAccordian.js
+++ assets/js/faqAccordian.js
@@ -3,12 +3,13 @@ export function accordianButtons() {
     btns.forEach((btn) => { // Each accordian
         btn.addEventListener('click', (accordian) => { // Event listener on each accordian button
             const answer = accordian.currentTarget.parentNode.parentNode.children[1]; // Parent of the answer 
-            const icon = answer.parentNode.children[0].children[1];
-            if (icon.classList.contains('fa-chevron-down')) {
-                answer.style.height = `${answer.children[0].offsetHeight}px`;
-                icon.classList.remove('fa-chevron-down');
-                icon.classList.add('fa-chevron-up');
-            } else if (icon.classList.contains('fa-chevron-up')) {
+            const icon = answer.parentNode.children[0].children[1]; // Open/Close accordian icon
+            const answerText = answer.children[0]; // Answer Text (paragraph element)
+            if (icon.classList.contains('fa-chevron-down')) { // If the accordian is closed
+                answer.style.height = `${answerText.offsetHeight}px`; // Dynamically sets the height of the paragraph container to match the height of the paragraph element (answerText)
+                icon.classList.remove('fa-chevron-down'); 
+                icon.classList.add('fa-chevron-up'); // Replaces the icon with an up arrow instead of a down arrow, to indicate that the accordian can be closed.
+            } else if (icon.classList.contains('fa-chevron-up')) { // If the accordian is open
                 answer.style.height = `0px`;
                 icon.classList.remove('fa-chevron-up');
                 icon.classList.add('fa-chevron-down');
